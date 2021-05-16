9e718642179786449521b33254bda6596c651ed1
sergiuvoicu <sergiuv28@yagoo.com>
2021-05-16 15:48:07 +0000

    melee levels system enemies follow

--- main.c
+++ main.c
@@ -26,7 +26,7 @@ uint32_t color(uint8_t red, uint8_t green, uint8_t blue) {
 static inline uint64_t rdtsc(void)
 {
     uint32_t hi, lo;
-    __asm__ ("rdtsc" : "=a" (lo), "=d" (hi));
+    __asm__ __volatile__ ("rdtsc" : "=a" (lo), "=d" (hi));
     return ((uint64_t) lo) | (((uint64_t) hi) << 32);
 }
 
@@ -37,74 +37,69 @@ void square(uint32_t *buf, uint32_t color, unsigned x, unsigned y, unsigned widt
 }
 
 
-int countEnemies( int enArr []){
 
-  int i = 0;
-  if(enArr == NULL)
-    return 0;
-  else{
-     while(enArr[i] != '\0')
-       ++i;
-  }
-
-  return i;
-}
-
-void deleteEnemy(uint32_t *buffer, int enArr [], int x, int y, int w2, int h2, int w){
-	for( int i = 0; i < countEnemies(enArr) ; i+=2){
-     
-    	if( abs(x-enArr[i]) < w2 &&  abs(y-enArr[i+1]) < h2){
-        	square(buffer, color(0xf4, 0x71, 0x42), enArr[i], enArr[i+1], w2, h2, w);
-        	enArr[i] = -1;
-        	enArr[i+1] = -1;
+void deleteEnemy( int enArr[], int n, int *num, int x, int y, int w2, int h2, int w){
 		
+	for( int i = 0; i < n ; i+=2){
+     
+    	if( abs(x-enArr[i]) < w2 &&  abs(y-enArr[i+1]) < h2 && enArr[i] != -100 && enArr[i+1] != -100){
+        	enArr[i] = -100;
+        	enArr[i+1] = -100;
+		*num -= 2;
     	}
     }
-
 }
 
-int countEnemiesRemained( int enArr[] ){
-	int count = 0;
-	if(enArr == NULL)
-		return 0;
-	else for(int i= 0; i< countEnemies(enArr); i+=2)
-		if(enArr[i] != -1)
-			++count;
-	return count;
+void moveEnemies( int enArr [], int n, int delta, int x, int y, int w2, int h2, int width, int height ){
+
+	for( int i = 0; i < n; i+=2)
+	   if(enArr[i] != -100){
+		int direction = (uint32_t) rdtsc()%100;
+         	if( direction <= 50){
+			if(enArr[i] < x)
+                	enArr[i]+= delta;
+			if(enArr[i] > x)
+			enArr[i] -= delta;
+			if( enArr[i] + w2 > width) enArr[i] = width-w2;
+			if( enArr[i] < 0 ) enArr[i] = 0;
+		}
+         	else if(direction > 50){
+			if(enArr[i+1] < y)
+                	enArr[i+1]+= delta;
+			if(enArr[i+1] > y)
+			enArr[i+1] -= delta;
+			if( enArr[i+1] < 0) enArr[i+1] = 0;
+			if( enArr[i+1] + h2 > height) enArr[i+1] = height-h2;
+         	}
+	   }
 }
 
 
 EFI_STATUS EFIAPI efi_main(EFI_HANDLE img, EFI_SYSTEM_TABLE *systab) {
   InitializeLib(img, systab);
-  //Print(L"This is a game\r\n");
 
   EFI_GRAPHICS_OUTPUT_PROTOCOL *gop;
+ // EFI_SIMPLE_POINTER_PROTOCOL *mouse = 0;
 
   EFI_STATUS status = LibLocateProtocol(&GraphicsOutputProtocol, (void**)&gop);
   CHECK(status, TRUE);
 
   int width = gop->Mode->Info->HorizontalResolution;
-  int height = gop->Mode->Info->VerticalResolution;
-
+  int height = gop->Mode->Info->VerticalResolution-100;
+ // gST->ConOut->EnableCursor(gST->ConOut,TRUE);
   if (gop->Mode->Info->PixelFormat != PixelBlueGreenRedReserved8BitPerColor) {
     Print(L"Error: don't know about pixel format %d\r\n", gop->Mode->Info->PixelFormat);
     Exit(EFI_SUCCESS, 0, NULL);
   }
 
-  Print(L"%dx%d\r\n", width, height);
-  //WaitForSingleEvent(systab->ConIn->WaitForKey, 0);
-
   uint64_t size = width * height * sizeof(EFI_GRAPHICS_OUTPUT_BLT_PIXEL);
   uint32_t *buffer = AllocateZeroPool(size);
 
-  int x = 200;
-  int y = 300;
+  int x = (uint32_t) rdtsc() % width;
+  int y = 450;
 
   int w1 = 25;
   int h1 = 25;
-
-  int x2 = 100;
-  int y2 = 100;
 
   int w2 = 25;
   int h2 = 25;
@@ -113,28 +108,46 @@ EFI_STATUS EFIAPI efi_main(EFI_HANDLE img, EFI_SYSTEM_TABLE *systab) {
 
   int level = 1;
   int lifes = 3;
-  uint32_t tax = (uint32_t) rdtsc() % 500;
-  int enemies[] = {(uint32_t) rdtsc()%400,(uint32_t) rdtsc()%400,(uint32_t) rdtsc() % 400 , (uint32_t) rdtsc %400}; 
-  //for( int i = 0; i<6; ++i){
-  //enemies[i] = rand();}
+   
+  int n = 0;
+  while(level <= 3 && lifes > 0){
+  if(level == 1)
+   n = 8;
+  if (level == 2)
+   n = 12;
+  else if (level == 3)
+   n = 20;
+
+  int enemies[n];
+  int num = n;
+
+  for( int i=0; i< n; ++i)
+	enemies[i] = 0;
+
+  for( int i = 0; i< n; i+=2){
+        enemies[i] = (uint32_t) rdtsc()%750;
+        enemies[i+1] = (uint32_t) rdtsc()%400;
+}
+  
 
 
       
-  while (TRUE) {
-    // background
+  while (num!=0) {
+
+    moveEnemies(enemies,n,10,x,y,w2,h2,width,height);
+  
+  // background
     square(buffer, color(0xf4, 0x71, 0x42), 0, 0, width, height, width);
-    // player
+  // player
     square(buffer, color(0x59, 0xf4, 0x42), x, y, w1, h1, width);
-    // enemy
-    
-    for( int i = 0 ; i< countEnemies(enemies); i+=2)
-       if(enemies[i] != -1 && enemies[i+1] != -1)
-         square(buffer, color(0, 0, 0xff), enemies[i], enemies[i+1], w2, h2, width);
-
-
-    status = uefi_call_wrapper(gop->Blt, 10, gop, buffer, EfiBltBufferToVideo, 0, 0, 0, 0, width, height, 0);
+   
+  // enemies
+    for( int i = 0 ; i< n; i+=2)
+       if(enemies[i] != -100 && enemies[i+1] != -100)
+	  square(buffer, color(0, 0, 0xff), enemies[i], enemies[i+1], w2, h2, width);
+	
+    status = uefi_call_wrapper(gop->Blt, 10, gop, buffer, EfiBltBufferToVideo,0,0,0,0, width, height, 0);
     CHECK(status, FALSE);
-
     WaitForSingleEvent(ST->ConIn->WaitForKey, 0);
     EFI_INPUT_KEY key;
     uefi_call_wrapper(ST->ConIn->ReadKeyStroke, 2, ST->ConIn, &key);
@@ -143,25 +156,21 @@ EFI_STATUS EFIAPI efi_main(EFI_HANDLE img, EFI_SYSTEM_TABLE *systab) {
     if (key.UnicodeChar == 'a') x -= delta;
     if (key.UnicodeChar == 's') y += delta;
     if (key.UnicodeChar == 'd') x += delta;
+    if (key.UnicodeChar == 'm') deleteEnemy(enemies,n,&num,x,y,w2,h2,width);    
 
     if (y < 0) y = 0;
     if (y + h1 > height) y = height-h1;
     if (x < 0) x = 0;
     if (x + w1 > width) x = width-w1;
 
-
-    if (y2 < 0) y2 = 0;
-    if (y2 + h2 > height) y2 = height-h2;
-    if (x2 < 0) x2 = 0;
-    if (x2 + w2 > width) x2 = width-w2;
-
-    deleteEnemy(buffer,enemies,x,y,w2,h2,width);
-   // if(countRemainedEnemies(enemies)==0 && enemies != NULL)
-	//++level;
+    if(num==0)
+	level+=1;
+  }
   }
 
   Print(L"press any key to exit...\r\n");
   WaitForSingleEvent(systab->ConIn->WaitForKey, 0);
-
+  
   return EFI_SUCCESS;
 }
+
